d5e4cf244bd076859b3a05656fc64957fb325aa3
Yuriy Zubaha <yuriy.zubaha@gmail.com>
2017-07-11 01:33:35 +0300

    Configuring Routes

---
+++ public/app/app.js
@@ -0,0 +1,45 @@
+(function() {
+
+    var app = angular.module('app', ['ngRoute']);
+
+    app.config(['$logProvider', '$routeProvider', function ($logProvider, $routeProvider) {
+
+        $logProvider.debugEnabled(true);
+
+        $routeProvider
+            .when('/', {
+                controller: 'HomeController',
+                controllerAs: 'home',
+                templateUrl: 'app/templates/home.html'
+            })
+            .when('/schools', {
+                controller: 'AllSchoolsController',
+                controllerAs: 'schools',
+                templateUrl: 'app/templates/allSchools.html'
+            })
+            .when('/classrooms', {
+                controller: 'AllClassroomsController',
+                controllerAs: 'classrooms',
+                templateUrl: 'app/templates/allClassrooms.html',
+            })
+            .when('/classrooms/:id', {
+                controller: 'AllClassroomsController',
+                controllerAs: 'classrooms',
+                templateUrl: 'app/templates/allClassrooms.html',
+                redirectTo: function(params, currPath, currSearch) {
+                    console.log(params);
+                    console.log(currPath);
+                    console.log(currSearch);
+                    return '/';
+                }
+            })
+            .when('/activities', {
+                controller: 'AllActivitiesController',
+                controllerAs: 'activities',
+                templateUrl: 'app/templates/allActivities.html'
+            })
+            .otherwise('/');
+
+    }]);
+
+}());
